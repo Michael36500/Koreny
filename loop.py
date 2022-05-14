c344325601b2b6525f56cc08c0581846196bb533
Michael36500 <104157791+Michael36500@users.noreply.github.com>
2022-05-14 15:33:57 +0200

    asdf

--- loop.py
+++ loop.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import os
 import cannyED as ed
+from tqdm import tqdm 
 
 # bake je funukce, co a) přepočítá obrázek b) uloží a znovu otevře obrázek
 def bake(hard = None):
@@ -14,14 +15,14 @@ def bake(hard = None):
 
 files = os.listdir("in")
 img_num = 0
-th_dos = 11
+th_dos = 40
 
-for actualf in files:
+for actualf in tqdm(files):
 
 # terminal output
     img_num = img_num + 1
-    print(img_num)
-    print("    ",actualf)
+    # print(img_num)
+    # print("    ",actualf)
 
 # img read + H,W
     img = cv2.imread("in/{}".format(actualf)).astype("float64") # 0 protože chci černobílý obrázek (pro krok úprava)
@@ -29,7 +30,7 @@ for actualf in files:
 
 # crop obrázků
         # zmenšit, oříznout SD / ořiznout 10K
-    zmenseni = True
+    zmenseni = False 
     if zmenseni == True:
         img = cv2.resize(img, (800, 600))                     # POZOR, ROZMĚRY SE NEPOČÍTAJÍ
         img = img[100 : 540, 70 : 700]        #crop malých, mnou zmenšeny
@@ -37,14 +38,15 @@ for actualf in files:
         img = img[1480 : 8000, 410 : 10700]        #crop velkých, originálů
     # cv2.imwrite("temp.png", img)
     # img = cv2.imread("temp.png")
-    bake()
-# efekty
-    # th, img = cv2.threshold(img, th_dos, 255, cv2.THRESH_BINARY)
+    bake(False)
+# th efekt
+    th, img = cv2.threshold(img, th_dos, 255, cv2.THRESH_BINARY)
+    bake(False)
 
 
 
 
-    img = ed.Canny_detector(img, 70)
+    # img = ed.Canny_detector(img, 70)
 
 
 
@@ -85,5 +87,8 @@ for actualf in files:
 # ukládání originálů
     # img = cv2.putText(img, str(min_cm), (0, 1000), cv2.FONT_HERSHEY_SIMPLEX, 40, (255, 255, 255), 40, cv2.LINE_AA)
     # img = cv2.putText(img, str(sum(plocha)), (0, 2000), cv2.FONT_HERSHEY_SIMPLEX, 40, (255, 255, 255), 40, cv2.LINE_AA)
-    cv2.imwrite("out/{}.jpg".format(actualf), img)
-    break
+    cv2.imwrite("out/{}".format(actualf), img)
+    
+    
+    
+    # break
