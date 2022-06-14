ddd6acb8e58f46df493e35d83194685ba7f6d8ee
Michael36500 <104157791+Michael36500@users.noreply.github.com>
2022-06-14 19:47:20 +0200

    asdf

--- def_filtrArea.py
+++ def_filtrArea.py
@@ -8,9 +8,10 @@ def filtrArea (cnts):
     def TrueFilter(cnts, s1):
         area_cnts = []
         for cnt in cnts:    
+            print(cnt)
         # osobně nevím, proč to takhle je, ale funguje to...
             cnt = list(cnt)
-            cnt.pop(-1)
+            # cnt.pop(-1)
             for cn in cnt:
                 # print(cn, "XXXXXXX")
                 if s1<cv2.contourArea(cn):
@@ -20,6 +21,7 @@ def filtrArea (cnts):
 
     # while True najde dynamicky ideální tresh plochy
 
+    # s1 -> základ thresholdu, a od něj jdu po 10 nahoru, dokud se nedostanu k THRESH (max. počet objektů, pokud jich je +THRESH, tak zvýším S1 o 10, znovu)
     s1 = 10
     thresh = 250
     while True:
--- main3.py
+++ main3.py
@@ -1,4 +1,3 @@
-import imghdr
 from multiprocessing.spawn import import_main_path
 import cv2
 import numpy as np
@@ -22,7 +21,13 @@ import def_threshold as thresh
 import def_filtrArea as filtrArea
 
 # nakreslí na 1. parametr pixelly/contours z 2. parametru
-import def_drawCNTs as drawCNTs
+# import def_drawCNTs as drawCNTs
+
+# importuje filtr tvary
+import def_filtrTvar as filtrTvar
+
+show_imgs = False
+os.system('cls')
 
 # najde všechny soubory v IN složce, aby se mohl loopnout
 inputs_jpg = os.listdir("in/")
@@ -32,40 +37,80 @@ for a in inputs_jpg:
     short = a[:l - 4]
     inputs.append(short)
 # vynechávám img_num, protože ho nepotřebuji
+if show_imgs == True:
+    cv2.namedWindow("win", cv2.WINDOW_AUTOSIZE)
 
-for actual_img in tqdm(inputs):
+# for actual_img in tqdm(inputs):
+for actual_img in inputs:
 
+    # load
     bw = cv2.imread("in/{}.jpg".format(actual_img), 0).astype("float64") # 0 protože chci černobílý obrázek (pro krok úprava)
     # bw = cv2.cvtColor(source, cv2.COLOR_RGB2GRAY)
     cv2.imwrite("debug/1.BW/{}.png".format(actual_img), bw)
-    # print("1.BW")
 
+    # also load original image to write on later
+    original = cv2.imread("in/{}.jpg".format(actual_img))
+    original = crop.crop(original)
+
+    print("BW")
+    if show_imgs == True:
+        frame = cv2.resize(bw, (979, 652))
+        cv2.imshow('win', frame)
+        cv2.waitKey(200)
+    
+
+    # criop
     cropped = crop.crop (bw)
     cv2.imwrite("debug/2.cropped/{}.png".format(actual_img), cropped)
-    # print("2.cropped")
-
-    # 10 je možno vyladit
-    threshed = thresh.th(cropped, 10)
-    cv2.imwrite("debug/3.thresh/{}.png".format(actual_img), threshed)
-    # print("3.thresh")
-
- 
     
-    raw_cnts = findCNTs.findCNTs(actual_img)
+    print("cropped")
+    if show_imgs == True:
+        frame = cv2.resize(cropped, (979, 652))
+        cv2.imshow('win', frame)
+        cv2.waitKey(200)
 
+    # threshold
+    threshed = thresh.th(cropped, 10)
+    # 10 je možno vyladit
+    cv2.imwrite("debug/3.thresh/{}.png".format(actual_img), threshed)
+
+    print("threshed")
+    if show_imgs == True:
+        frame = cv2.resize(threshed, (979, 652))
+        cv2.imshow('win', frame)
+        cv2.waitKey(200)
+
+    # raw CNTs
+        # musim bejknout
+    threshed = bake.bake(threshed, False)
+    raw_cnts = findCNTs.findCNTs(threshed)
+
+    # print(raw_cnts)
+    
+    # area  
     area_cnts = filtrArea.filtrArea(raw_cnts)
 
-    # print(area_cnts)
+    # # cv2.drawContours(, area_cnts, -1, (255, 2255, 255), 3)
+    # area_img = cv2.drawContours(original, area_cnts, -1, (255, 255 ,255), 3)
 
-    cnt_cropped = drawCNTs.drawCNTs(cropped, area_cnts)
-    # print(cnts)
+    # cv2.imwrite("debug/4.CNTs/{}.png".format(actual_img), area_img)
 
-    cv2.drawContours(cnt_cropped, area_cnts, -1, (255, 2255, 255), 3)
-    cv2.imwrite("debug/4.CNTs/{}.png".format(actual_img), cnt_cropped)
+    # if show_imgs == True:
+    #     frame = cv2.resize((area_img), (979, 652))
+    #     cv2.imshow('win', frame)
+    #     cv2.waitKey(200)
+    # # tvar filtr
+    # tvar_area_cnts = filtrTvar.filtrTvar(area_cnts, threshed)
+
+    # # chybí excentricitou
+
+    # cnt_cropped = drawCNTs.drawCNTs(cropped, area_cnts)
+    # # print(cnts)
+
 
 
 
 
 
     
-    # break
+    break
