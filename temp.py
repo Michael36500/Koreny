15f5d0f857b8d378431e19a7a7a8e76cda89fd29
Michael36500 <104157791+Michael36500@users.noreply.github.com>
2023-01-30 20:30:27 +0100

    sync

--- temp.py
+++ temp.py
@@ -3,5 +3,6 @@ for x in range(11):
     for y in range(11):
         y /= 10
         for z in range(10):
+            z += 1
             z *= 100
-        print(x, y, z)
+            print("[", [x], ", ", [y], ", ", [z], "]", ", ", sep="")
